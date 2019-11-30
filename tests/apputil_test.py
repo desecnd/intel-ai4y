121295897964ef9f6080ad54730d0665006b80b9
tomdol <tomasz.dolbniak@intel.com>
2019-11-30 19:14:00 +0100

    Gather 'append_word' tests in a class

--- tests/apputil_test.py
+++ tests/apputil_test.py
@@ -1,32 +1,33 @@
 from utils import apputil
 
-def test_append_word_to_empty_message():
-    current_message = ""
-    word_to_append = "word"
-    new_message = apputil.append_word(current_message, word_to_append)
-    
-    assert(new_message == word_to_append + " ")
-    
-def test_append_word_to_word():
-    current_message = "word1 "
-    word_to_append = "word2"
-    new_message = apputil.append_word(current_message, word_to_append)
-    
-    expected_message = current_message + word_to_append + " "
-    assert(new_message == expected_message)
+class TestAppendWord:
+    def test_append_word_to_empty_message(self):
+        current_message = ""
+        word_to_append = "word"
+        new_message = apputil.append_word(current_message, word_to_append)
+        
+        assert(new_message == word_to_append + " ")
+        
+    def test_append_word_to_word(self):
+        current_message = "word1 "
+        word_to_append = "word2"
+        new_message = apputil.append_word(current_message, word_to_append)
+        
+        expected_message = current_message + word_to_append + " "
+        assert(new_message == expected_message)
 
-def test_autocomplete_last_word():
-    current_message = "my test mes"
-    word_to_append = "message"
-    new_message = apputil.append_word(current_message, word_to_append)
+    def test_autocomplete_last_word(self):
+        current_message = "my test mes"
+        word_to_append = "message"
+        new_message = apputil.append_word(current_message, word_to_append)
 
-    expected_message = "my test message "
-    assert(new_message == expected_message)
+        expected_message = "my test message "
+        assert(new_message == expected_message)
 
-def test_dont_autocomplete_last_word():
-    current_message = "my test message"
-    word_to_append = "message"
-    new_message = apputil.append_word(current_message, word_to_append)
+    def test_dont_autocomplete_last_word(self):
+        current_message = "my test message"
+        word_to_append = "message"
+        new_message = apputil.append_word(current_message, word_to_append)
 
-    expected_message = "my test message message "
-    assert(new_message == expected_message)
+        expected_message = "my test message message "
+        assert(new_message == expected_message)
