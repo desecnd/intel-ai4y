4fa82b77a3d97af62bb19bf97044ca21276cbbd9
tomdol <tomasz.dolbniak@intel.com>
2019-12-01 18:12:33 +0100

    Open all windows upon startup

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ class Config:
         self.hand_cols = self.training_cols * 10
         self.video_width = 640
         self.video_height = 480
+        self.enabled_windows = ('video', 'Letter Prediction', 'Word Suggestions',
+                                'Hand Gesture', 'Skeleton on hand')
 
 class Renderer:
-    def __init__(self, config, windows):
-        self.window_names = windows
+    def __init__(self, config):
+        self.config = config
+
+        self.open_windows()
         
         self.cap = cv2.VideoCapture(0)
         self.cap.set(3, config.video_width)
@@ -27,15 +31,29 @@ class Renderer:
 
     def render(self, images):
         for win, img in images.items():
-            if win in self.window_names:
+            if win in self.config.enabled_windows:
                 cv2.imshow(win, img)
+
+    def open_windows(self):
+        blank_img = numpy.zeros((100, 100))
+        self.render({'video': blank_img,
+                     'Hand Gesture': blank_img,
+                     'Skeleton on hand': blank_img,
+                     'Letter Prediction': blank_img,
+                     'Word Suggestions': blank_img})
+                     
+        cv2.moveWindow('video', 500, 125)
+        cv2.moveWindow('Hand Gesture', 0, 0)
+        cv2.moveWindow('Skeleton on hand', 0, 450)
+        cv2.moveWindow('Letter Prediction', 1200, 0)
+        cv2.moveWindow('Word Suggestions', 1200, 450)
+
 
 class Predictor:
     def __init__(self):
         pass
 
     def predict(self):
-        print("predicting")
         time.sleep(1)
         return numpy.random.randn(100, 100)
 
@@ -44,32 +62,27 @@ class Program(Thread):
         self.config = config
         self.stop_event = stop_event
         self.character_predictor = Predictor()
-        self.renderer = Renderer(config, ('main_window'))
+        self.renderer = Renderer(config)
         Thread.__init__(self)
 
     def run(self):
-        print("starting")
         while not stop_event.is_set():
             img = self.character_predictor.predict()
-            self.renderer.render({'main_window': img})
+            self.renderer.render({'video': img})
 
 if __name__ == "__main__":
     stop_event = Event()
     config = Config()
+
     program = Program(stop_event, config)
-    cv2.imshow('main_window', numpy.zeros((100, 100)))
     program.daemon = True
     program.start()
 
     while (True):
-        print("waiting for user input")
         userChoice = chr(cv2.waitKey(0) & 255)
-        print("key pressed: ", userChoice)
 
         if userChoice == 'q':
-            print("sending stop event")
             stop_event.set()
-            print("closing windows")
             cv2.destroyAllWindows()
             break;
 
